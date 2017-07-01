d44924809e667b8a112a44bf768e2cefafaa3586
rootid <visawant@gmail.com>
2017-07-01 18:47:36 -0400

    updated cleanup script

--- README.txt
+++ README.txt
@@ -2,3 +2,4 @@
 
 export VI_CONF=$HOME/vi2.config
 source ~/vi2.config/mybash.sh
+
--- mybash.sh
+++ mybash.sh
@@ -2,8 +2,11 @@ ENV=dev
 
 if [ "${ENV}" == "dev" ]
 then
-  source ${VI_CONF}/bash.addons/default.shrc
+	#echo "DEV env invoked"
+	echo ""
 fi
+
+source ${VI_CONF}/bash.addons/default.shrc
 source ${VI_CONF}/bash.addons/git.shrc
 source ${VI_CONF}/bash.addons/search.shrc
 source ${VI_CONF}/bash.addons/ack.shrc
